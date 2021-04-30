9d4f4e2baff61308d7b7eaf73dc2fc5113057d3a
Alex Rivera <alexriveracruz4@gmail.com>
2021-04-30 00:31:54 +0000

    [ADD] calculator of args

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, exit
+
+    counter = len(argv) - 1
+    if counter != 3:
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    s = argv[2]
+
+    if s == "+":
+        print("{} {} {} = {}".format(a, s, b, add(a, b)))
+    elif s == "-":
+        print("{} {} {} = {}".format(a, s, b, sub(a, b)))
+    elif s == "*":
+        print("{} {} {} = {}".format(a, s, b, mul(a, b)))
+    elif s == "/":
+        print("{} {} {} = {}".format(a, s, b, siv(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
