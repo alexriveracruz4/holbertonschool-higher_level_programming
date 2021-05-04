1151dbdc344ebaedc93726175cf55bcd24736018
Alex Rivera <alexriveracruz4@gmail.com>
2021-05-04 17:53:45 +0000

    [ADD] funtion print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in range(len(row)):
-            if i < len(row) - 1:
-                print("{:d}".format(row[i]), end=" ")
-            else:
-                print("{:d}".format(row[i]))
+        print(" ".join("{:d}".format(i) for i in row))
