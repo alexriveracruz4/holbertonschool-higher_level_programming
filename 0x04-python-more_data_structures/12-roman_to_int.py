6c0468c0d5e5a610e039ad8a14dbd0e7346cb5f4
Alex Rivera <alexriveracruz4@gmail.com>
2021-05-06 21:04:04 +0000

    [ADD] function roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,6 +4,11 @@ def roman_to_int(roman_string):
         return(0)
 
     value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    for i in roman_string:
+        if i not in value.keys():
+            return(0)
+
     list1 = [value[i] for i in roman_string]
     num = 0
 
