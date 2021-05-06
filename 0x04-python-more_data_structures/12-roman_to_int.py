835dfed22c8ab651a8328be30ca600b9f1c98fa7
Alex Rivera <alexriveracruz4@gmail.com>
2021-05-06 21:42:08 +0000

    [ADD] function roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,19 +3,19 @@ def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return(0)
 
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rom_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
     for i in roman_string:
-        if i not in roman_dict.keys():
+        if i not in rom_dic.keys():
             return(0)
     num = 0
 
     for i in range(len(roman_string)):
         if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
+                rom_dic[roman_string[i]] < rom_dic[roman_string[i + 1]]):
+                num += rom_dic[roman_string[i]] * -1
 
         else:
-            num += roman_dict[roman_string[i]]
+            num += rom_dic[roman_string[i]]
 
     return(num)
