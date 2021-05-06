b6c9f13594f72b51ca12f3f1ab4aa5150e69ae39
Alex Rivera <alexriveracruz4@gmail.com>
2021-05-06 21:39:25 +0000

    [ADD] function roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if isinstance(roman_string, str) is False or roman_string is None:
+    if type(roman_string) is not str or roman_string is None:
         return(0)
 
-    value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
     for i in roman_string:
-        if i not in value.keys():
+        if i not in roman_dict.keys():
             return(0)
-
-    list1 = [value[i] for i in roman_string]
     num = 0
 
-    if len(list1) == 1:
-        return(list1[0])
+    for i in range(len(roman_string)):
+        if (i != (len(roman_string) - 1) and
+                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+                num += roman_dict[roman_string[i]] * -1
 
-    for j in range(len(list1) - 1):
-        if list1[j] >= list1[j + 1]:
-            num += list1[j]
-            if j is (len(list1) - 2):
-                num += list1[j + 1]
         else:
-            num += list1[j + 1] - list1[j]
+            num += roman_dict[roman_string[i]]
+
     return(num)
