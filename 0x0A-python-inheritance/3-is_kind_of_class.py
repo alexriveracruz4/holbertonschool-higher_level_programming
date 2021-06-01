ffd9d8bfbb5ca4a86eae56275e3203f8da86e420
Alex Rivera <alexriveracruz4@gmail.com>
2021-06-01 21:15:03 +0000

    [ADD] function is kind of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Define is_kind_f_class function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return true if onj is instance or instance inherited from"""
+    return(isinstance(obj, a_class))
