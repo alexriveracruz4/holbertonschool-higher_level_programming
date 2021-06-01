c5e05d0d052accee4f5b6ae59254ab6f5a2b0e92
Alex Rivera <alexriveracruz4@gmail.com>
2021-06-01 23:43:04 +0000

    [modify] typo

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,5 +1,5 @@
-#!usr/bin/python3
-"""Define BaseGeomtry Class
+#!/usr/bin/python3
+"""Define BaseGeometry Class
 Define Rectangle Subclass"""
 
 
@@ -9,7 +9,7 @@ BaseGeometry = __import__("7-base_geometry").BaseGeometry
 class Rectangle(BaseGeometry):
     """Represent a rectangle"""
     def __init__(self, width, height):
-        """Initializr the Rectangle"""
+        """Initialize the Rectangle"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
