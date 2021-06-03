c4834fac003475d532540c6ee5953cf4c344b5a3
Alex Rivera <alexriveracruz4@gmail.com>
2021-06-03 02:35:48 +0000

    [ADD] function pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines pascal_triangle function"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers representing
+    the Pascal’s triangle of n"""
+
+    if n <= 0:
+        return([])
+
+    listTriangle= [[1]]
+    while len(listTriangle) < n:
+        last = listTriangle[-1]
+        add = [1]
+        for i in range(len(last) - 1):
+            add.append(last[i] + last[i + 1])
+        add.append(1)
+        listTriangle.append(add)
+    return(listTriangle)
