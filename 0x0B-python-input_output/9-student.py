f9dcd8a57917beb538d31dad792daf58c791cfa2
Alex Rivera <alexriveracruz4@gmail.com>
2021-06-03 01:45:54 +0000

    [ADD] class student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines Student class"""
+
+
+class Student:
+    """Represent a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initialize tha class Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student instance"""
+        return(self.__dict__)
