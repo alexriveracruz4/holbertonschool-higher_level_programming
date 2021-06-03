46b7a4938ff1b3ecf505cfb0a5d403837dd289fd
Alex Rivera <alexriveracruz4@gmail.com>
2021-06-03 02:15:38 +0000

    [ADD] class student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Defines Student class"""
+
+
+class Student:
+    """Represent a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initialize tha class Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return(self.__dict__)
+        newDict = {}
+        for at in attrs:
+            try:
+                newDict[at] = self.__dict__[at]
+            except:
+                pass
+        return newDict
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for key in json:
+            try:
+                setattr(self, key, json[key])
+            except:
+                pass
