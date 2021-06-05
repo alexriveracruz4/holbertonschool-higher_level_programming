b4b42e3259f9bbf8a47e810bbf5b13f125c944ed
Alex Rivera <alexriveracruz4@gmail.com>
2021-06-05 18:09:43 +0000

    [ADD] class rectangle with errors

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,42 +13,58 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
-        @property
-        def width(self):
-            """Getter of width"""
-            return(self.__width)
+    @property
+    def width(self):
+        """Getter of width"""
+        return(self.__width)
 
-        @width.setter
-        def width(self, value):
-            """Setter of width"""
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """Setter of width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            """Getter of height"""
-            return(self.__height)
+    @property
+    def height(self):
+        """Getter of height"""
+        return(self.__height)
 
-        @height.setter
-        def height(self, value):
-            """Setter of height"""
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """Setter of height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
-        @property
-        def x(self):
-            """Getter of x"""
-            return(self.__x)
+    @property
+    def x(self):
+        """Getter of x"""
+        return(self.__x)
 
-        @x.setter
-        def x(self, value):
-            """Setter of x"""
-            self.__x = value
+    @x.setter
+    def x(self, value):
+        """Setter of x"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
-        @property
-        def y(self):
-            """Getter of y"""
-            return(self.__y)
+    @property
+    def y(self):
+        """Getter of y"""
+        return(self.__y)
 
-        @y.setter
-        def y(self, value):
-            """Setter of y"""
-            self.__y = value
+    @y.setter
+    def y(self, value):
+        """Setter of y"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
