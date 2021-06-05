783b37d4ed2ebe79c9a16c52377782b02ea44173
Alex Rivera <alexriveracruz4@gmail.com>
2021-06-05 17:19:10 +0000

    [ADD] class Rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Define Rectangle Class"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Represent Rectangle class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize Rectangle class"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+        @property
+        def width(self):
+            """Getter of width"""
+            return(self.__width)
+
+        @width.setter
+        def width(self, value):
+            """Setter of width"""
+            self.__width = value
+
+        @property
+        def height(self):
+            """Getter of height"""
+            return(self.__height)
+
+        @height.setter
+        def height(self, value):
+            """Setter of height"""
+            self.__height = value
+
+        @property
+        def x(self):
+            """Getter of x"""
+            return(self.__x)
+
+        @x.setter
+        def x(self, value):
+            """Setter of x"""
+            self.__x = value
+
+        @property
+        def y(self):
+            """Getter of y"""
+            return(self.__y)
+
+        @y.setter
+        def y(self, value):
+            """Setter of y"""
+            self.__y = value
