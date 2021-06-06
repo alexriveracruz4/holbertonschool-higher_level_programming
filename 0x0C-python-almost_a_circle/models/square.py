fabd785a06bd397735ca86dcb2358f96bcc6bb9d
Alex Rivera <alexriveracruz4@gmail.com>
2021-06-06 00:24:55 +0000

    [ADD] public method update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -24,3 +24,25 @@ class Square(Rectangle):
         """Setter of size"""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """Method that assigns attributes"""
+        if len(args):
+            for i, a in enumerate(args):
+                if i == 0:
+                    self.id = a
+                elif i == 1:
+                    self.size = a
+                elif i == 2:
+                    self.x = a
+                elif i == 3:
+                    self.y = a
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
