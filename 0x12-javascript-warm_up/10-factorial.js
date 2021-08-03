dfe53edecee6fc8399095d1d0549aff4c66e282d
Alex Rivera <alexriveracruz4@gmail.com>
2021-08-03 02:23:29 +0000

    [ADD] function factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+'use strict';
+const n = process.argv[2];
+function factorial (n) {
+  if (isNaN(n) || n === 1) {
+    return (1);
+  } else {
+    return (n * factorial(n - 1));
+  }
+}
+console.log(factorial(Number(n)));
