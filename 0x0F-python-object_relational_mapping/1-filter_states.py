e4e5cfd9d2a5d2fd6978983cb56d96f762021925
Alex Rivera <alexriveracruz4@gmail.com>
2021-08-07 16:05:11 +0000

    [UPDATE] search for N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,11 +8,8 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states \
-                      WHERE name LIKE 'N%' ORDER BY id ASC ")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-    cursor.close()
+    c = db.cursor()
+    c.execute("SELECT * FROM states ORDER BY id")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+    c.close()
     db.close()
