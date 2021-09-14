1fc671f5688800395208af45905a8d5687a91ca2
Alex Rivera <alexriveracruz4@gmail.com>
2021-09-14 20:55:28 +0000

    [ADD] status of url

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log('code: ' + response.statusCode);
+});
